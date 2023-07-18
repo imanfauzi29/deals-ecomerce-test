4e4122f4abc433e6d04cbab0bf3b920ec1768819
iman fauzi <imanfauzi29@gmail.com>
2023-07-18 07:29:17 +0700

    ✏️ Fix cart title wording

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <>
       <div>
         <div className="flex justify-between w-full mb-5">
-          <h1 className="font-semibold text-xl">Product</h1>
+          <h1 className="font-semibold text-xl">Cart</h1>
         </div>
         <CartTable />
       </div>
